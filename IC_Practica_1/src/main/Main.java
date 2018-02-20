06affa88ccb461b3cc28862570e1861247741b0d
Juan <koldur.c@gmail.com>
2018-02-20 17:27:27 +0100

    First commit. A* started

---
+++ IC_Practica_1/src/main/Main.java
@@ -0,0 +1,14 @@
+package main;
+
+import javax.swing.JFrame;
+
+import view.MainView;
+
+public class Main {
+
+	public static void main(String[] args) {
+		JFrame m = new MainView();
+		m.setVisible(true);
+	}
+
+}
---
+++ IC_Practica_1/src/view/MainView.java
@@ -0,0 +1,76 @@
+package view;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.GridLayout;
+import java.awt.Rectangle;
+import java.awt.event.ComponentEvent;
+
+import javax.swing.BorderFactory;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.SwingConstants;
+
+public class MainView extends JFrame {
+
+	/**
+	 *  generated to supress warnings
+	 */
+	private static final long serialVersionUID = 1L;
+
+	private JPanel body;
+	
+	public MainView() {
+		super();
+	    this.setTitle("Practica 1 - Juan G-Martinho");
+		this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		this.setResizable(true);
+		this.setSize(500,500);
+		this.setResizable(false);
+		this.setMinimumSize(new Dimension(200,200));
+		body = new JPanel(new BorderLayout());
+		
+		generateNorth();
+		generateSouth();
+		generateEast();
+		generateWest();
+		generateCenter();
+		
+		this.add(body);
+	}
+	
+	private void generateNorth() {
+		JLabel north = new JLabel("North", SwingConstants.CENTER);
+		body.add(north, BorderLayout.NORTH);
+	}
+	private void generateSouth() {
+		JLabel south = new JLabel("South", SwingConstants.CENTER);
+		body.add(south, BorderLayout.SOUTH);
+	}
+	private void generateEast() {
+		JLabel east = new JLabel("East", SwingConstants.CENTER);
+		body.add(east, BorderLayout.EAST);
+	}
+	private void generateWest() {
+		JLabel west = new JLabel("West", SwingConstants.CENTER);
+		body.add(west, BorderLayout.WEST);
+	}
+	private void generateCenter() {
+		
+		JPanel center = new JPanel(new GridLayout(6,6));
+		
+		for(int i = 0; i < 6*6; i++) {
+			JLabel label = new JLabel("");
+		    label.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+		    label.setBackground(Color.white);
+		    label.setOpaque(true);
+		    center.add(label);
+		}
+		center.setMinimumSize(new Dimension(200,200));
+		
+		body.add(center, BorderLayout.CENTER);
+		
+	}
+}
