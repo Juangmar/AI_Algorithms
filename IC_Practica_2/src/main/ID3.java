705273998936125aeb7c96f966b50c356afe7cd9
Juan <koldur.c@gmail.com>
2018-04-23 19:00:49 +0200

    Main class commented

--- IC_Practica_2/src/main/ID3.java
+++ IC_Practica_2/src/main/ID3.java
@@ -1,14 +1,15 @@
 package main;
 
 import javax.swing.JFrame;
-
 import view.DataSelectorView;
 
+/**
+ * @author Juan Gómez-Martinho
+ *
+ */
 public class ID3 {
-
 	public static void main(String[] args) {
-		JFrame m = new DataSelectorView();
-		m.setVisible(true);
+		JFrame m = new DataSelectorView(); //The program window is created.
+		m.setVisible(true); //The window is now visible.
 	}
-
 }
