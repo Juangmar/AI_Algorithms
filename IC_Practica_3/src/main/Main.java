091ec21da401c6d7ee81af9148f24e0a4ccd6849
Juan <koldur.c@gmail.com>
2018-04-24 15:18:35 +0200

    Practica_3 first commit
    Project created

---
+++ IC_Practica_3/src/main/Main.java
@@ -0,0 +1,13 @@
+package main;
+
+import javax.swing.JFrame;
+import view.SelectorFrame;
+
+public class Main {
+
+	public static void main(String[] args) {
+		JFrame window = new SelectorFrame();
+		window.setVisible(true);
+	}
+
+}
---
+++ IC_Practica_3/src/view/SelectorFrame.java
@@ -0,0 +1,22 @@
+package view;
+
+import javax.swing.JFrame;
+
+/**
+ * @author Juan Gómez-Martinho
+ *
+ */
+public class SelectorFrame extends JFrame {
+
+	/**
+	 *  Default serial version
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public SelectorFrame() {
+		super();
+		this.setTitle("Clasificador");
+		this.setSize(200,100);
+		this.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+	}
+}
